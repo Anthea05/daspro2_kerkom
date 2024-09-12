4c6ad214a60706851a916d449818488ba383208a
IzzaDhafira <izzadhafirafanani@gmail.com>
2024-09-12 23:10:54 +0700

    PerkenalanDiriSaya10

---
+++ PerkenalanDiriSaya10.java
@@ -0,0 +1,18 @@
+package Pertemuan2;
+
+public class PerkenalanDiriSaya10 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi-POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Izza Dhafira Fanani");
+        System.out.println("NIM\t\t: 244107020106");
+        System.out.println("Kelas\t\t: 1G");
+        System.out.println("No Absen\t: 10");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: Perum Permata Jingga Blok Sawit No. 18");
+        System.out.println("No. HP\t\t: 081217929132");
+        System.out.println("Hobi\t\t: Mendengarkan musik");
+    }
+}
+
