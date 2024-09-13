343e22ce5a4a03dd7004384119982790d6ae90db
ayuannisa17 <aryaniannisa4@gmail.com>
2024-09-13 09:01:34 +0700

    PerkenalanDiriSaya04

---
+++ PerkenalanDiriSaya04.java
@@ -0,0 +1,17 @@
+package pertemuan2;
+
+public class PerkenalanDiriSaya04 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi-POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Ayu Aannisa Aryani Nagari");
+        System.out.println("NIM\t\t: 244107020185");
+        System.out.println("Kelas\t\t: 1G");
+        System.out.println("No Absen\t: 04");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: Jl bunga merak II no 16");
+        System.out.println("No. HP\t\t: 0813331184792");
+        System.out.println("Hobi\t\t: Memasak");
+    }
+}
